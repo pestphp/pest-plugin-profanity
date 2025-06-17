c1d3d12d2e414c9d724a3f766cd1d830250c4e55
UnMelow <Unmelow@yandex.ru>
2025-06-18 02:05:47 +0300

    feat: sort Russian profanity list and add simple test

---
+++ tests/Fixtures/HasExplicitRussianProfanity.php
@@ -0,0 +1,8 @@
+<?php
+namespace Tests\Fixtures;
+
+class HasExplicitRussianProfanity
+{
+    /** @var string */
+    public string $bad = 'Это полная хуйня!';
+}
---
+++ tests/Fixtures/HasMaskedRussianProfanity.php
@@ -0,0 +1,8 @@
+<?php
+namespace Tests\Fixtures;
+
+class HasMaskedRussianProfanity
+{
+    /** @var string */
+    public string $masked = 'Ну и п@зда же ты, x*й тебе!';
+}
